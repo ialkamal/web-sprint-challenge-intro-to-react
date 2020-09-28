bd20a51129027e347ed27723cc43c08f5b3fc1f8
Ismail Al Kamal <ismail@nawatt.com>
2020-09-28 06:22:43 +0300

    MVP complete

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+const CharacterCard = styled.div`
+  background: #99f3eb;
+  color: black;
+  width: 350px;
+  max-height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 20px auto;
+`;
+
 // Write your Character component here
+function Character({ name, birth_year }) {
+  return (
+    <CharacterCard>
+      <h2>{`${name} ${birth_year}`}</h2>
+    </CharacterCard>
+  );
+}
+
+export default Character;
